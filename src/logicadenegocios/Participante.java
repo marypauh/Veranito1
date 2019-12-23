e80922397fe58dfc8674a76de5c3c4c77173f3e3
raquelrojas <raquelrojascr@gmail.com>
2019-12-23 12:30:33 -0600

    listo

--- src/logicadenegocios/Participante.java
+++ src/logicadenegocios/Participante.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+/**
+ * Clase de los objetos de tipo Participante
+ * 
+ * @author Kevin Castillo, Ma Paula Rodriguez y Raquel Rojas
+ * @version 1.0
  */
 package logicadenegocios;
 
-/**
- *
- * @author María Paula
- */
 public class Participante {
+    private String nombre;
+    private String email;
+    
+    public Participante(String pNombre, String pEmail){
+      setNombre(pNombre);
+      setEmail(pEmail);
+    }
+
+    
+    public String getNombre() {
+      return nombre;
+    }
+
+    
+    public String getEmail() {
+      return email;
+    }
+
+    
+    private void setNombre(String pNonbre) {
+      this.nombre = pNonbre;
+    }
+
+    
+    private void setEmail(String pEmail) {
+      this.email = pEmail;
+    }
+    
+      
+    /**
+   * Método para convertir en String toda la información del Participante
+   */
+    @Override
+    public String toString(){
+    String msg = "Nombre: " + getNombre() + "email: " + getEmail();
+    return msg;     
+    }
+    
+    
+    
     
 }
+
